7d6dd1cccbe9bcfee5d1e5a8692a46a0b0bf9370
Artur <artik_andronov@mail.ru>
2024-09-17 19:10:55 +0300

    tags if, for, url

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,25 +4,29 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-menu = ["About page", "Add article", "Feed back", "Login"]
+menu = [
+    {'title': 'About page', 'url_name': 'about'},
+    {'title': 'Add article', 'url_name': 'add_page'},
+    {'title': 'Feedback', 'url_name': 'contact'},
+    {'title': 'Login', 'url_name': 'login'}
+]
+
+data_db = [
+    {'id': 1, 'title': ' Angelina Jolie', 'content': 'Biography of Angelina Jolie', 'is_published': True},
+    {'id': 2, 'title': 'Margo Robie', 'content': 'Biography of Margo Robie', 'is_published': False},
+    {'id': 3, 'title': 'Julie Roberts', 'content': 'Biography of Julie Roberts', 'is_published': True},
+]
 
 class MyClass:
     def __init__(self, a,b):
         self.a = a
         self.b = b
 
-def index(request): #HttpRequest
-    # t = render_to_string("women/index.html")
-    # return HttpResponse(t)
+def index(request):
     data = {
-        'title': 'main page?',
+        'title': 'Main page',
         'menu': menu,
-        'float': 28.56,
-        'lst': [1, 2, 'abc', True],
-        'set': {1,2,3,2,5},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10,20),
-        'url': slugify("The main page")
+        'posts': data_db,
             }
     return render(request, "women/index.html", context = data)
 
@@ -32,17 +36,14 @@ def about(request):
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Categories</h1><p>id: {cat_id}</p>")
 
-def categories_by_slug(request, cat_slug):
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"displaying an article with id = {post_id}")
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport', ))
-        return HttpResponsePermanentRedirect(uri)
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
-
+def addpage(request):
+    return HttpResponse("adding article")
+def contact(request):
+    return HttpResponse("Feedback")
+def login(request):
+    return HttpResponse("Authorization")
 def page_not_found(request, exception):
     return HttpResponseNotFound("Page not found")
