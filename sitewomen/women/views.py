bc7c30d3ff867752b9e07f3d141b22e901412ef6
Artur <artik_andronov@mail.ru>
2024-10-17 18:26:17 +0300

    loading files with using models

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 from pip._vendor.rich.markup import Tag
 
 from women.forms import AddPostForm, UploadFileForm
-from women.models import Women, Category, TagPost
+from women.models import Women, Category, TagPost, UploadFiles
 
 menu = [
     {'title': 'About page', 'url_name': 'about'},
@@ -31,16 +31,17 @@ def index(request):
             }
     return render(request, "women/index.html", context = data)
 
-def handle_uploaded_file(f):
-    with open(f"uploads/{f.name}", "wb+") as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
+# def handle_uploaded_file(f):
+#     with open(f"uploads/{f.name}", "wb+") as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)
 
 def about(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(form.cleaned_data['file'])
+            fp = UploadFiles(file=form.cleaned_data['file'])
+            fp.save()
     else:
         form = UploadFileForm()
     return render(request, "women/about.html",
@@ -63,7 +64,7 @@ def show_post(request, post_slug):
 
 def addpage(request):
    if request.method == "POST":
-       form = AddPostForm(request.POST)
+       form = AddPostForm(request.POST, request.FILES)
        if form.is_valid():
            #print(form.cleaned_data)
             # try:
