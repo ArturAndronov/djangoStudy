6f502df35b0cd920de454283e489cb20e8515120
Artur <artik_andronov@mail.ru>
2024-11-02 11:33:00 +0200

    Template context processors

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,8 +1,13 @@
 from django import template
 import women.views as views
 from women.models import Category, TagPost
+from women.utils import menu
 
 register = template.Library()
+
+@register.simple_tag
+def get_menu():
+    return menu
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -2,7 +2,6 @@ menu = [
     {'title': 'About page', 'url_name': 'about'},
     {'title': 'Add article', 'url_name': 'add_page'},
     {'title': 'Feedback', 'url_name': 'contact'},
-    {'title': 'Login', 'url_name': 'login'}
 ]
 
 class DataMixin:
@@ -15,10 +14,7 @@ class DataMixin:
             self.extra_context['title'] = self.title_page
         if self.cat_selected is not None:
             self.extra_context['cat_selected'] = self.cat_selected
-        if 'menu' not in self.extra_context:
-            self.extra_context['menu'] = menu
     def get_mixin_context(self, context, **kwargs):
-        context['menu'] = menu
         context['cat_selected'] = None
         context.update(kwargs)
         return context
